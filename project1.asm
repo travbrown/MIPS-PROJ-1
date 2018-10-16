b3eaea5eddb8eed2f6fd3ac0be7fb8786b98b8da
Travis Brown <travjbrown98@gmail.com>
2018-10-15 20:57:00 -0400

    printed 2 characters, only loading  once

--- project1.asm
+++ project1.asm
@@ -4,6 +4,14 @@
 .text 
 .globl  main
 main:
+	li $v0, 11
+
+        la $a0, 66 # B (66)
+        syscall
+
+        addi $a0, $a0, 48 # r (114)
+        syscall
+	
 	li $v0, 10	# The exit system call 
 	syscall		# Calling the OS to perfom execution
 	
