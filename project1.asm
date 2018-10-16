bc7b932bee286f72c4475dfe41b5b2884196394a
Travis Brown <travjbrown98@gmail.com>
2018-10-16 15:18:53 -0400

    Print @ & 1st 4 ID digits

--- project1.asm
+++ project1.asm
@@ -5,12 +5,30 @@
 .globl  main
 main:
 	li $v0, 11
-
-        la $a0, 66 # B (66)
+	
+	la $a0, 64 # @ (64)
         syscall
 
-        addi $a0, $a0, 48 # r (114)
+        li $v0, 1
+
+        addi $a0, $a0, -64 # 0
         syscall
+
+        addi $a0, $a0, 2 # 2
+        syscall
+
+        addi $a0, $a0, 6 # 8
+        syscall
+
+        addi $a0, $a0, -6 # 2
+        syscall
+        syscall
+
+        #la $a0, 66 # B (66)
+        #syscall
+
+        #addi $a0, $a0, 48 # r (114)
+        #syscall
 	
 	li $v0, 10	# The exit system call 
 	syscall		# Calling the OS to perfom execution
