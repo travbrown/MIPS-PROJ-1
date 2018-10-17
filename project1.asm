95d31185dce8d30c500cc2fa7a94591e25ab7edf
Travis Brown <travjbrown98@gmail.com>
2018-10-17 11:15:18 -0400

    Printed the last 3 digits in my ID number

--- project1.asm
+++ project1.asm
@@ -4,12 +4,12 @@
 .text 
 .globl  main
 main:
-	li $v0, 11
+	li $v0, 11 # print-character system call
 	
-	la $a0, 64 # @ (64)
-        syscall
+	la $a0, 64 # @ (64) 
+        syscall	# OS will know to print the ASCII code equivalent to the decimal number in $a0
 
-        li $v0, 1
+        li $v0, 1 # print-integer system call
 
         addi $a0, $a0, -64 # 0
         syscall
@@ -22,6 +22,13 @@ main:
 
         addi $a0, $a0, -6 # 2
         syscall
+        syscall
+
+	addi $a0, $a0, 7 # 9
+        syscall
+        syscall
+
+        addi $a0, $a0, -5 # 4
         syscall
 
         #la $a0, 66 # B (66)
