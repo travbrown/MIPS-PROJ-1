de74d34e877be51ec8ccff44e43a4e6993b90709
Travis Brown <travjbrown98@gmail.com>
2018-10-18 14:22:40 -0400

    added and organized documentation/comments, printed LF & first few letters of last name

--- project1.asm
+++ project1.asm
@@ -1,42 +1,48 @@
 #	TRAVIS BROWN  - MIPS PROJECT 1
-.data
-
 .text 
 .globl  main
 main:
-	li $v0, 11 # print-character system call
+	li $v0, 11	# print-character system call
 	
-	la $a0, 64 # @ (64) 
-        syscall	# OS will know to print the ASCII code equivalent to the decimal number in $a0
+	la $a0, 64	# @ (64) 
+        syscall		# OS will know to print the ASCII code equivalent to the decimal number in $a0
 
-        li $v0, 1 # print-integer system call
+        li $v0, 1	# print-integer system call
 
-        addi $a0, $a0, -64 # 0
+        addi $a0, $a0, -64	# 0
         syscall
 
-        addi $a0, $a0, 2 # 2
+        addi $a0, $a0, 2	# 2
         syscall
 
-        addi $a0, $a0, 6 # 8
+        addi $a0, $a0, 6	# 8
         syscall
 
-        addi $a0, $a0, -6 # 2
+        addi $a0, $a0, -6	# 2
         syscall
+        syscall			# prints the same value again
+
+	addi $a0, $a0, 7 	# 9
+        syscall
+        syscall			# prints the same value again
+
+        addi $a0, $a0, -5 	# 4
         syscall
 
-	addi $a0, $a0, 7 # 9
-        syscall
-        syscall
+	li $v0, 11
 
-        addi $a0, $a0, -5 # 4
+        addi $a0, $a0, 6 	# LF or new line (10)
         syscall
 
-        #la $a0, 66 # B (66)
-        #syscall
+        addi $a0, $a0, 56 	# B (66)
+        syscall
 
-        #addi $a0, $a0, 48 # r (114)
-        #syscall
+        addi $a0, $a0, 48 	# r (114)
+        syscall
+
+	addi $a0, $a0, -3 	# o (111)
+        syscall
 	
-	li $v0, 10	# The exit system call 
-	syscall		# Calling the OS to perfom execution
+	li $v0, 10		# The exit system call 
+	syscall			# Calling the OS to perfom execution
 	
